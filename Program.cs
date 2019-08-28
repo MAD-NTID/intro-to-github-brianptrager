6c31bb7eeef7e8ab0cef7116294cab29236c741a
Brian Trager <brian.p.trager@outlook.com>
2019-08-28 09:17:04 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Dot Net is awesome!!");
+            Console.WriteLine("I can't wait to show you more!");
+        }
+    }
+}
